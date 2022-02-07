faadbd88f5329a856b95fd159e78e5578a864182
nsegur66 <nico.segura44@gmail.com>
2022-02-07 21:31:23 +0100

    récupération d ela branch main

--- pages/categories.vue
+++ pages/categories.vue
@@ -1,52 +1,55 @@
 <template>
-  <section>
-    <h1>test recup categories</h1>
-    <p v-for="(category, index) of categories" :key="index">
-      {{ category.name }}
-    </p>
-  </section>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-img
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      height="200px"
+    ></v-img>
+
+    <v-card-title>
+      Top western road trips
+    </v-card-title>
+
+    <v-card-subtitle>
+      1,000 miles of wonder
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn
+        color="orange lighten-2"
+        text
+      >
+        Explore
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        icon
+        @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
 </template>
 
 <script>
-import api from "../store/api";
-
-export default {
-  name: "categories",
-  data() {
-    return {
-      categories: [
-        {
-          name: "Species",
-          link: "species",
-          img: "species.jpg",
-        },
-        {
-          name: "Film",
-          link: "films",
-          img: "films.jpg",
-        },
-        {
-          name: "Vehicle",
-          link: "vehicles",
-          img: "vehicles.png",
-        },
-        {
-          name: "People",
-          link: "people",
-          img: "people.jpg",
-        },
-        {
-          name: "Starship",
-          link: "starships",
-          img: "starships.jpg",
-        },
-        {
-          name: "Planet",
-          link: "planets",
-          img: "planets.jpg",
-        },
-      ],
-    };
-  },
-};
+  export default {
+    data: () => ({
+      show: false,
+    }),
+  }
 </script>
