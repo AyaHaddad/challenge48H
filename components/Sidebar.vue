83baf271cb71334ab1e0a83be5a310a5af10ee3a
MCEBILE <mathis.cebile@ynov.com>
2022-02-07 22:49:58 +0100

    Update Sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -30,11 +30,11 @@
     data () {
       return {
         items: [
-          { title: 'People', icon: 'mdi-account-cowboy-hat-outline' },
-          { title: 'Planet', icon: 'mdi-earth' },
+          { title: 'People', icon: 'mdi-account-group' },
+          { title: 'Planets', icon: 'mdi-earth' },
           { title: 'Starships', icon: 'mdi-space-station' },
           { title: 'Films', icon: 'mdi-movie-open' },
-          { title: 'Vehicule', icon: 'mdi-car-hatchback' },
+          { title: 'Vehicules', icon: 'mdi-car-hatchback' },
           { title: 'Species', icon: 'mdi-alien-outline' },
         ]
       }
