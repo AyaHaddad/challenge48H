1fea4c3c3fe6250db8a456c276b313b6cb95f516
AyaHaddad <aya.haddad@ynov.com>
2022-02-07 17:11:37 +0100

    [ADD]style scss + axios get people

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
+  <v-app>
+    <Nuxt />
+    <!-- <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
       :clipped="clipped"
@@ -84,35 +85,35 @@
       app
     >
       <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+    </v-footer> -->
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'DefaultLayout',
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
-}
+  name: "DefaultLayout",
+  // data () {
+  //   return {
+  //     clipped: false,
+  //     drawer: false,
+  //     fixed: false,
+  //     items: [
+  //       {
+  //         icon: 'mdi-apps',
+  //         title: 'Welcome',
+  //         to: '/'
+  //       },
+  //       {
+  //         icon: 'mdi-chart-bubble',
+  //         title: 'Inspire',
+  //         to: '/inspire'
+  //       }
+  //     ],
+  //     miniVariant: false,
+  //     right: true,
+  //     rightDrawer: false,
+  //     title: 'Vuetify.js'
+  //   }
+  // }
+};
 </script>
