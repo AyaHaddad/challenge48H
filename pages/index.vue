740e790a29488b63d916c62508282b2156d4bb32
MCEBILE <mathis.cebile@ynov.com>
2022-02-07 18:25:52 +0100

    fix bug

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,6 @@
 <template>
-        <div>
-     <nav class="navbar navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">
-    <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-    Quizz
-  </a>
-</nav>
+    <div>
+
     </div>
 </template>
 
