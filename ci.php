f11f727d8ec4cf0c6603fd524da5923ebf0828b0
mawindu <mawen.carole@gmail.com>
2022-02-08 23:58:48 +0100

    test ci 5

--- ci.php
+++ ci.php
@@ -1,8 +1,8 @@
-echo 'Bonjour'
-#rm -fr /var/www/challenge48h/
-#git clone git@github.com:AyaHaddad/challenge48h.git
-#MDP
-#cd /var/www/challenge48h/
-#npm i
-#npm run build
-#systemctl reload apache2
+<?php
+
+  $data = print_r($_SERVER, true).json_decode(file_get_contents('php://input'));
+  file_put_contents('./logs/'.time().'.txt', $data);
+
+
+
+?>
