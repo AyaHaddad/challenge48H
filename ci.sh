e70ed861088c59e881ae6397c0ee25c210f349c1
mawindu <mawen.carole@gmail.com>
2022-02-08 23:12:00 +0100

    test ci 2

--- ci.sh
+++ ci.sh
@@ -1,4 +1,8 @@
 echo 'Bonjour'
+#rm -fr /var/www/challenge48h/
+#git clone git@github.com:AyaHaddad/challenge48h.git
+#MDP
+#cd /var/www/challenge48h/
 #npm i
 #npm run build
 #systemctl reload apache2
