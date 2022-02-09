53fd9a27a9e31f0c4edf1aafd141554b666e304e
Alex <rolland.alexandre2@gmail.com>
2022-02-09 04:28:32 +0100

    [ADD] Text

--- components/HomeBlockQuiz.vue
+++ components/HomeBlockQuiz.vue
@@ -1,7 +1,7 @@
 <template>
 <section>
     <h1 class="title">Want to test your knowledge ?</h1>
-    <p class="description">You can visit our database to find out about the different items on the star saga.ndard dummy</p>
+    <p class="description">You can visit our database to find out about the different items on the star saga.</p>
     <v-btn class="px-10" ><NuxtLink to="/quizHome">Quiz</NuxtLink></v-btn>
 </section>
 </template>
--- components/HomeBlockWiki.vue
+++ components/HomeBlockWiki.vue
@@ -1,7 +1,8 @@
 <template>
 <section>
     <h1 class="title">Welcome to Star Wars library</h1>
-    <p class="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
+    <p class="description">All the Star Wars data you've ever wanted: Planets, Spaceships, Vehicles, People, Films and Species. From all SEVEN Star Wars films. Now with The Force Awakens data!
+</p>
     <v-btn class="px-10"><NuxtLink to="/category">Wiki</NuxtLink></v-btn>
 </section>
 </template>
